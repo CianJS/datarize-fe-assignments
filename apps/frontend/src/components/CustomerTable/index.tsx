624a979eedfd16a7596180f76ab17b7fccc3fc94
CianJS <erwinousy@gmail.com>
2024-08-17 17:03:25 +0900

    총 구매 횟수를 기준으로 테이블 정렬 기능 추가

--- apps/frontend/src/components/CustomerTable/index.tsx
+++ apps/frontend/src/components/CustomerTable/index.tsx
@@ -3,7 +3,9 @@ import { Table, Header, HeaderRow, Body, Row, HeaderCell, Cell } from '@table-li
 
 import { useTheme } from '@table-library/react-table-library/theme'
 import { getTheme } from '@table-library/react-table-library/baseline'
-import { ChangeEvent, InputHTMLAttributes, useState } from 'react'
+import { HeaderCellSort, useSort } from '@table-library/react-table-library/sort'
+import { ChangeEvent, useState } from 'react'
+
 interface ChartProps {
   data: Customer[]
   onSelect: (item: Customer) => void
@@ -26,6 +28,21 @@ function CustomerDetailTable({ data, onSelect }: ChartProps) {
       }
     `,
   })
+  const sort = useSort(
+    data,
+    {
+      onChange: () => {},
+    },
+    {
+      sortFns: {
+        TASK: (array: Customer[]) => array.sort(() => 1),
+        DEADLINE: (array: Customer[]) => array.sort(() => 1),
+        TYPE: (array: Customer[]) => array.sort(() => 1),
+        COMPLETE: (array: Customer[]) => array.sort(() => 1),
+        TASKS: (array: Customer[]) => array.sort((a, b) => a.count - b.count),
+      },
+    },
+  )
   const [selectedCustomerId, setSelectedCustomerId] = useState<number>()
   const [search, setSearch] = useState<string>('')
 
@@ -48,14 +65,18 @@ function CustomerDetailTable({ data, onSelect }: ChartProps) {
 
       <br />
 
-      <Table data={{ nodes: data.filter((item) => (search ? item.name.search(search) > -1 : true)) }} theme={theme}>
+      <Table
+        data={{ nodes: data.filter((item) => (search ? item.name.search(search) > -1 : true)) }}
+        sort={sort}
+        theme={theme}
+      >
         {(tableList: Customer[]) => (
           <>
             <Header>
               <HeaderRow>
                 <HeaderCell>고객ID</HeaderCell>
                 <HeaderCell>고객명</HeaderCell>
-                <HeaderCell>총 구매 횟수</HeaderCell>
+                <HeaderCellSort sortKey="TASKS">총 구매 횟수</HeaderCellSort>
                 <HeaderCell>총 구매 금액</HeaderCell>
               </HeaderRow>
             </Header>
